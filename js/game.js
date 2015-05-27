da077294c834cf2566ea35dca9a3945d548b02b9
Camilla Borg <camillaborg88@gmail.com>
2015-05-27 11:38:26 +0200

    bokstäver skrivs i varje ruta

--- js/game.js
+++ js/game.js
@@ -6,6 +6,12 @@ console.log(valtOrd);
 var button = document.getElementsByTagName("button")[0]; // Vi vill ha tillbaka den första knappen button[plats0]
 button.addEventListener("click", function(){
     var användarOrd = document.getElementsByTagName("input")[0].value; //Hämtar värdet från inputfältet och sparar i var
+
+    var rutor = document.getElementsByTagName("td");
+    for(var i = 0; i < 5; i++){
+        rutor[i].innerHTML = användarOrd[i];
+    }
+
     if(valtOrd === användarOrd){
         alert("GRATTIS!");
     }
