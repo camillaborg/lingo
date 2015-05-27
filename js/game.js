b962f12d8f5acf6e8328be28687e2ddf06e5ecae
Camilla Borg <camillaborg88@gmail.com>
2015-05-27 15:59:32 +0200

    Uppdaterad funktion med 3 gissningar

--- js/game.js
+++ js/game.js
@@ -1,22 +1,43 @@
 var ordlista = ["mjauu", "malin", "banan"];
 var valtOrd = ordlista[Math.floor(Math.random() * 3)];
+var counts = 3;
+var tries = 0;
 console.log(valtOrd);
 // Slumpar ett ord från arrayen
 
 var button = document.getElementsByTagName("button")[0]; // Vi vill ha tillbaka den första knappen button[plats0]
-button.addEventListener("click", function(){
-    var användarOrd = document.getElementsByTagName("input")[0].value; //Hämtar värdet från inputfältet och sparar i var
 
-    var rutor = document.getElementsByTagName("td");
-    for(var i = 0; i < 5; i++){
-        rutor[i].innerHTML = användarOrd[i];
 
-        if(användarOrd[i] === valtOrd[i]){
-            rutor[i].style.background = "green";
+
+    button.addEventListener("click", function () {
+
+        if(tries < counts) {
+            tries++
+        var användarOrd = document.getElementsByTagName("input")[0].value; //Hämtar värdet från inputfältet och sparar i var
+
+        var rutor = document.getElementsByTagName("td");
+        for (var i = 0; i < 5; i++) {
+            rutor[i].innerHTML = användarOrd[i];
+
+            if (användarOrd[i] === valtOrd[i]) {
+                rutor[i].style.background = "green";
+            }
+            else {
+                rutor[i].style.background = "red";
+            }
         }
-        else{
-            rutor[i].style.background = "red";
+
+
+        if (användarOrd === valtOrd) {
+            alert("Grattis!")
         }
+        else if (tries < counts) {
+            alert("Försök igen");
+        }
+        else if (tries >= counts) {
+            alert("Dina försök är slut");
+        }
+
     }
+    });
 
-});
